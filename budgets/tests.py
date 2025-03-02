01a057d75e400dea14a8fa0a198bf225178408c3
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-02 19:52:09 -0300

    Testes aplicados nas urls e views

--- budgets/tests.py
+++ budgets/tests.py
@@ -1,7 +1,36 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+
+from budgets import views
 
 # Create your tests here.
 
 class BudgetURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+    def test_budget_home_url_is_correct(self):
+        home_url = reverse('budgets:home')
+        self.assertEqual(home_url, '/')
+
+    def test_budget_category_url_is_correct(self):
+        url = reverse('budgets:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/budgets/category/1/')
+
+    def test_budget_detail_url_is_correct(self):
+        url = reverse('budgets:budget', kwargs={'id': 1})
+        self.assertEqual(url, '/budgets/1/')
+
+class BudgetViewsTest(TestCase):
+    def test_budget_home_view_function_is_correct(self):
+        view = resolve(reverse('budgets:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_budget_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('budgets:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, views.category)
+
+    def test_budget_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('budgets:budget', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.budget)
