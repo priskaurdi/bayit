83fe09fddddbe7937cf43e44ee4b0f52467f681c
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-02 20:56:48 -0300

    Criando testes error 200 ou 404

--- budgets/tests/test_budget_views.py
+++ budgets/tests/test_budget_views.py
@@ -11,14 +11,41 @@ class BudgetViewsTest(TestCase):
         view = resolve(reverse('budgets:home'))
         self.assertIs(view.func, views.home)
 
+    def test_budget_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('budgets:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_budget_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('budgets:home'))
+        self.assertTemplateUsed(response, 'budgets/pages/home.html')
+    
+    def test_budget_home_template_shows_no_budgets_found_if_no_budgets(self):
+        response = self.client.get(reverse('budgets:home'))
+        self.assertIn(
+            '<h1>No budgets found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
+    
     def test_budget_category_view_function_is_correct(self):
         view = resolve(
-            reverse('budgets:category', kwargs={'category_id': 1})
+            reverse('budgets:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
+
+    def test_budget_category_view_returns_404_if_no_budgets_found(self):
+        response = self.client.get(
+            reverse('budgets:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_budget_detail_view_function_is_correct(self):
         view = resolve(
             reverse('budgets:budget', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.budget)
+    
+    def test_budget_detail_view_returns_404_if_no_budgets_found(self):
+        response = self.client.get(
+            reverse('budgets:budget', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
