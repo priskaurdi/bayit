3b3ebff66bb196543469169b5b4c3cf6cb6b040e
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-02 12:19:35 -0300

    Criando o superuser para o admin-django

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 # budgets:budget
-app_name = 'budgets'
+#app_name = 'budgets'
 
 urlpatterns = [
-    path('/', views.home, name="budgets-home"),
+    path('', views.home, name="budgets-home"),
     path('budgets/<int:id>/', views.budget, name="budget"), #budgets-budget
 ]
