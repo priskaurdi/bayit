8ad371d914853af0bf78f86f8f733d5c4e18a2eb
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-04 11:04:25 -0300

    Formularios .py e logout, login_message

---
+++ authors/forms/budget_form.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+from django import forms
+from django.core.exceptions import ValidationError
+
+from authors.validators import AuthorBudgetValidator
+from budgets.models import Budget
+from utils.django_forms import add_attr
+
+
+class AuthorBudgetForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
+
+    class Meta:
+        model = Budget
+        fields = 'title', 'description', 'preparation_time', \
+            'preparation_time_unit', 'servings', 'servings_unit', \
+            'preparation_steps', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('Pessoas', 'Pessoas'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                ),
+            ),
+        }
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        AuthorBudgetValidator(self.cleaned_data, ErrorClass=ValidationError)
+        return super_clean
