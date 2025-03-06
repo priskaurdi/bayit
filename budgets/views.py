27f138efd229a16316c37afe9f448edd776ef054
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-06 20:54:48 -0300

    Corrigindo a paginação do site

--- budgets/views.py
+++ budgets/views.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.budgets.factory import make_budget
+#from utils.budgets.factory import make_budget
 from utils.pagination import make_pagination
 
 from .models import Budget
@@ -12,14 +12,14 @@ from .models import Budget
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 def home(request):
-    budgets = get_list_or_404(
-            Budget.objects.filter(
+    budgets = Budget.objects.filter(
             is_published=True
         ).order_by('-id')
-    )
+    
     page_obj, pagination_range = make_pagination(request, budgets, PER_PAGE)
     return render(request, 'budgets/pages/home.html', context={
         'budgets': page_obj,
+        'pagination_range': pagination_range      
     })
 
 def category(request, category_id):
@@ -32,14 +32,12 @@ def category(request, category_id):
     page_obj, pagination_range = make_pagination(request, budgets, PER_PAGE)
     return render(request, 'budgets/pages/category.html', context={
         'budgets': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{budgets[0].category.name} - Category | '
     })
 
 def budget(request, id):
-    budget = get_object_or_404(Budget,
-        pk=id,
-        is_published=True,
-    )
+    budget = get_object_or_404(Budget, pk=id, is_published=True,)
 
     return render(request, 'budgets/pages/budget-view.html', context={
         'budget': budget,
@@ -64,4 +62,6 @@ def search(request):
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
         'budgets': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
