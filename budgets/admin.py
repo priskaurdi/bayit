0ccabbe3362205c7c0c28e10b20a740ea2fceb2d
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-21 18:35:42 -0300

    Ajustando o admin, painel

--- budgets/admin.py
+++ budgets/admin.py
@@ -10,7 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Budget)
 class BudgetAdmin(admin.ModelAdmin):
-    ...
-
+    list_display = ['id', 'title', 'created_at', 'is_publish']
+    list_display_link = ['title', 'created_at']
 
 admin.site.register(Category, CategoryAdmin)
