c6cc7a7cce00c8a452b6619ac74e2e3b29dd86d7
Priscila Nascimento <priskaurdi@gmail.com>
2025-03-30 13:29:03 -0300

    Problemas com o debug_toolbar, local erro db

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -24,7 +25,7 @@ urlpatterns = [
     path('', include('budgets.urls')),
     path('authors/', include('authors.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-]
+] + debug_toolbar_urls()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
